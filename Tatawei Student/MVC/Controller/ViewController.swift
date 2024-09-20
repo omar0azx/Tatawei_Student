6f9f4e65a9015712cd4784c1601bd60868e8739d
omar0azx <a3mr.azx@gmail.com>
2024-09-21 01:41:07 +0300

    first commit

---
+++ Tatawei Student/MVC/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tatawei Student
+//
+//  Created by omar alzhrani on 18/03/1446 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
