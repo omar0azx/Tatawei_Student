66bd36d3c747769673f48af89047989972b44598
omar0azx <a3mr.azx@gmail.com>
2024-10-27 07:07:00 +0300

    Add: condition for apply to opportunity depends on is student registered in school

--- Tatawei Student/Coordinator/MainCoordinator.swift
+++ Tatawei Student/Coordinator/MainCoordinator.swift
@@ -156,12 +156,7 @@ final class MainCoordinator: Coordinator {
         let vc = PreviousOpportunitiesVC.instantiate()
         vc.coordinator = self
         vc.modalPresentationStyle = .fullScreen
-        // Present the MapVC modally from the currently presented view controller
-        if let topViewController = navigationController.presentedViewController?.presentedViewController {
-            topViewController.present(vc, animated: true, completion: nil)
-        } else {
-            navigationController.present(vc, animated: true, completion: nil)
-        }
+        self.navigationController.present(vc, animated: true)
     }
 
     func viewAboutVC() {
